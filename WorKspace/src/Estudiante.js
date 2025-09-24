2a806c58966acd975c15524d76d469b7e25e8476
Dylan171111 <dylocampo6@ielasnieves.edu.co>
2025-09-24 16:29:14 -0500

    Add files via upload

---
+++ WorKspace/src/Estudiante.js
@@ -0,0 +1,16 @@
+import './Estudiante.css';
+
+function Estudiante(){
+    return(
+        <div className='estudiante'>
+            <div className='nombre'>
+                pepito
+            </div>
+            <div className='apellido'>
+                perez
+            </div>
+        </div>
+    )
+}
+
+export default Estudiante; 
